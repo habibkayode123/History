55c99f03dd1d4a9ae8c366a3947bca829aafdde0
Habib Popoola <habibkyodenew@gmail.com>
2020-09-10 21:07:38 +0200

    App updated

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Hello from "./componet/home";
+import History from "./componet/history";
+import Purchase from "./componet/purchase";
+
+export type RootStackParamList = {
+  Home: undefined;
+  History: undefined;``
+  Purchase: { purchaseId: string };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Hello}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="History" component={History} />
+        <Stack.Screen name="Purchase" component={Purchase} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
